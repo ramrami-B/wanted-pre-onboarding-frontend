c5d3cc5c768cfd3cf2a53dcb74f38d7e80832275
ramrami-B <21801034@handong.ac.kr>
2023-08-05 17:55:14 +0900

    [feat] todo 페이지

---
+++ src/assets/todo.style.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Li = styled.li`
+  list-style: none;
+  margin: 0 auto;
+`;
--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,12 +1,24 @@
+import * as S from "../assets/todo.style";
 import { useEffect } from "react";
 
 export function ToDo() {
+  const todos = ["TODO 1", "TODO 2"];
   useEffect(() => {
     if (!localStorage.getItem("access_token")) window.location.href = "/signin";
   }, []);
+
   return (
-    <div>
-      <h1>TODO</h1>
-    </div>
+    <S.Layout>
+      {todos.map((todo, idx) => {
+        return (
+          <S.Li key={idx}>
+            <label>
+              <input type="checkbox" />
+              <span>{todo}</span>
+            </label>
+          </S.Li>
+        );
+      })}
+    </S.Layout>
   );
 }
